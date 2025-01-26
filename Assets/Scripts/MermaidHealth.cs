db74fc17b174123fe81e4768869bbc7fd0fdc1ae
BoBo262 <79087084+BoBo262@users.noreply.github.com>
2025-01-26 13:18:42 +0200

    siren damage implemented

--- Assets/Scripts/MermaidHealth.cs
+++ Assets/Scripts/MermaidHealth.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class MermaidHealth : MonoBehaviour
 {
+
+    [SerializeField] public float health;
+    public DamageSink damageSink;
     // Start is called before the first frame update
     void Start()
     {
-        
+        damageSink = gameObject.GetComponent<DamageSink>();
+        damageSink.hp = health;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<BallBehaviour>())
+        {
+            BallBehaviour bullet = collision.gameObject.GetComponent<BallBehaviour>();
+            damageSink.TakeDamage(bullet.damage);
+            Destroy(bullet.gameObject);
+            if (damageSink.destroyed)
+            {
+                OnDestroy();
+            }
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Siren Is Killed");
+        Destroy(gameObject);
+        
+
     }
 }
